876ff75ed5c951347f49108216f7aa48bcb3dc51
ivan <ibodnar.dev@gmail.com>
2025-05-06 08:46:24 -0300

    k8s version == 1.29

--- terraform/modules/eks/variables.tf
+++ terraform/modules/eks/variables.tf
@@ -13,7 +13,7 @@ variable "cluster_name" {
 variable "cluster_version" {
   description = "Kubernetes version to use for the EKS cluster"
   type        = string
-  default     = "1.28"
+  default     = "1.29"
 }
 
 variable "vpc_id" {
